c503f1a08edec9a407f82292adb112b3bb4a687c
xddongx <31644115+xddongx@users.noreply.github.com>
2018-09-08 16:28:47 +0900

    퀴즈

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,19 @@
 var str = '111';
-       console.log(str);
+    console.log(str);
+
+for(var i=1;i<101;i++) {
+    document.writeln(i);
+    //console.log(i);
+}
+
+for(var i=1;i<101;i++){
+    if(i%2)
+    document.writeln(i);
+}
+
+for(var  i=2;i<=9;i++){
+    document.writeln(i+"단입니다.");
+    for(var k = 1;k<=9;k++){
+        document.writeln(i+"X"+k+"="+ i*k);
+    }
+}
