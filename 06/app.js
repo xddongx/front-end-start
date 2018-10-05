7e9898a932a3ce6fd17f3a8a51211191ade207e6
xddongx <31644115+xddongx@users.noreply.github.com>
2018-10-05 17:05:40 +0900

    hmmmm....

--- 06/app.js
+++ 06/app.js
@@ -1,1 +1,21 @@
-console.log('app')
+console.log('app');
+
+var appkey = '18da6e9b69375ae2e0f843494be7ee87';
+var query = '제주대학교';
+var url = `https://dapi.kakao.com/v2/search/web?query=${query}`;
+
+
+
+
+var myHeaders = new Headers();
+myHeaders.append('Authorization', 'KakaoAK 18da6e9b69375ae2e0f843494be7ee87');
+var options = {
+    headers: myHeaders
+}
+
+fetch(url, options)
+  .then(function(response){
+    response.json().then(function(data){
+      console.log('json data:',data);
+    });
+  })
