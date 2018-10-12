abeb0e2105c9d5192748658f98e8c133724418f1
xddongx <31644115+xddongx@users.noreply.github.com>
2018-10-12 18:07:58 +0900

    hmmmm......haaaaa....

---
+++ 07/jquery.js
@@ -0,0 +1,38 @@
+// document.querySelector('h1').innerHTML = 'hi';
+
+//  $('h1').fadeOut().delay(1000).fadeIn();
+
+// var $div = $('div')
+
+// $div
+//   .css('background-color', 'yellow')
+//   .css('border', '1px solid red');
+
+
+
+// var divs = document.querySelectorAll('div');
+// for(var i=0 ; i < divs.length; i++) {
+//   divs[i].style.backgroundColor = 'red';
+// }
+
+var $btn = $('#btn');
+var $txt = $('#txt');
+var $debug = $('#debug');
+
+$btn.on('click', function(event){
+  console.log('click')
+
+  $('h1').toggle('fast');
+//   if($txt.val() === '') {
+//     alert('값을 넣어주세요')
+//     return;
+//   }
+
+//   $debug.html($txt.val());
+
+});
+
+$('.wrap').on('click', 'div', function (event) {
+    $(event.target).fadeOut();
+    //console.log(event.target);
+})
